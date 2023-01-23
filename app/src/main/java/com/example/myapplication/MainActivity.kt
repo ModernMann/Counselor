a2cee4c4f1d747608b9e870f618268170d5e2374
VladimirObodnikov <VORON4325@gmail.com>
2023-01-23 20:34:39 +0300

    Add DataBase

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mDBHelper: DatabaseHelper? = null
+    private val mDb: SQLiteDatabase? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mDBHelper =  DatabaseHelper(this)
+        try {
+            mDBHelper!!.updateDataBase()
+        }
+        catch (e: Exception) {
+            Toast.makeText(this,"Невозможно обновить БД",Toast.LENGTH_SHORT).show()
+        }
     }
 }
